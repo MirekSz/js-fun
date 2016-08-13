cadbe61226e8cec94844ee15ffd2a5b6672ff050
Filip Olszewski <filipolszew@o2.pl>
2016-08-13 12:17:24 +0200

    rearrangement of code

--- 1_classic/users.js
+++ 1_classic/users.js
@@ -1,20 +1,40 @@
+var formHtml = ["<div class=\"row\"><div class=\"col-md-4\">&nbsp;<\/div><div class=\"col-md-4\"><h3>",
+    "<\/h3><form name=\"form\" id=\"form\"><div class=\"form-group\"><label for=\"name\">Imie:<\/label>" +
+    "<input id=\"name\" class=\"form-control\" type=\"text\"><\/div><div class=\"form-group\">" +
+    "<label for=\"surname\">Nazwisko:<\/label><input id=\"surname\" class=\"form-control\" type=\"text\"><\/div>" +
+    "<div class=\"form-group\"><label for=\"age\">Wiek:<\/label><input id=\"age\" class=\"form-control\" type=\"text\"><\/div>" +
+    "<div class=\"form-group\"><label for=\"sex\">Płeć:<\/label><select class=\"form-control\" id=\"sex\">" +
+    "<option>Mężczyzna<\/option><option>Kobieta<\/option><\/select><\/div><div class=\"form-group\">" +
+    "<input type=\"submit\" class=\"btn btn-primary\" value=\"Zapisz\">" +
+    "<button type=\"button\" id=\"cancelBtn\" class=\"btn btn-warning\">Cofnij<\/button><\/div><\/form><\/div>" +
+    "<div class=\"col-md-4\">&nbsp;<\/div><\/div>"];
+
 var users = [["Jacek", "Doe", "43", "Mężczyzna"],
     ["Marzanna", "Uss", "54", "Kobieta"], ["Julia", "Dolej", "22", "Kobieta"]];
-
-
 var isAnyRowSelected;
 var selectedRow;
+
+$(document).ready(initialize);
+
+function initialize() {
+    getData();
+}
+
+function getData() {
+    setTimeout(createTableView(users), 300);
+}
 
 function createTableView(users) {
     clearWorkspace();
     isAnyRowSelected = false;
     selectedRow = -1;
+
     var buttonHtml = "<button type=\"button\" class=\"btn btn-primary\" id=\"addBtn\">Dodaj<\/button> " +
         "<button type=\"button\" class=\"btn btn-primary\" id=\"editBtn\" disabled>Popraw<\/button> " +
         "<button type=\"button\" class=\"btn btn-primary\" id=\"deleteBtn\" disabled>Usuń<\/button>";
 
     $("#workspace").html(buttonHtml + prepareTableHtml(users) + '<div id="extendedView"></div>');
-    setOnClickForButtons();
+    setOnClickForTableViewButtons();
 }
 
 function prepareTableHtml(users) {
@@ -37,7 +57,6 @@ function prepareTableHtml(users) {
 }
 
 function onRowClick(rowNumber) {
-
     if (rowNumber == selectedRow) return;
     else {
         selectRow(rowNumber);
@@ -71,7 +90,7 @@ function showExtendedView(rowNumber) {
         + extViewHtml[1] + user[1] + extViewHtml[2] + user[2] + extViewHtml[3] + user[3] + extViewHtml[4]);
 }
 
-function setOnClickForButtons() {
+function setOnClickForTableViewButtons() {
     $("#addBtn").click(function () {
         clearWorkspace();
         createFormView("Dodaj");
@@ -94,7 +113,7 @@ function clearWorkspace() {
 
 function createFormView(mode) {
     $("#workspace").html(formHtml[0] + mode + " Użytkownika" + formHtml[1]);
-    if(mode == "Edytuj"){
+    if (mode == "Edytuj") {
         var user = users[selectedRow];
         $("#name").val(user[0]);
         $("#surname").val(user[1]);
@@ -136,24 +155,3 @@ function deleteUser() {
     isAnyRowSelected = false;
     createTableView(users);
 }
-
-function initialize() {
-    getData();
-}
-function getData() {
-    setTimeout(createTableView(users), 300);
-}
-
-$(document).ready(initialize);
-
-
-var formHtml = ["<div class=\"row\"><div class=\"col-md-4\">&nbsp;<\/div><div class=\"col-md-4\"><h3>",
-    "<\/h3><form name=\"form\" id=\"form\"><div class=\"form-group\"><label for=\"name\">Imie:<\/label>" +
-    "<input id=\"name\" class=\"form-control\" type=\"text\"><\/div><div class=\"form-group\">" +
-    "<label for=\"surname\">Nazwisko:<\/label><input id=\"surname\" class=\"form-control\" type=\"text\"><\/div>" +
-    "<div class=\"form-group\"><label for=\"age\">Wiek:<\/label><input id=\"age\" class=\"form-control\" type=\"text\"><\/div>" +
-    "<div class=\"form-group\"><label for=\"sex\">Płeć:<\/label><select class=\"form-control\" id=\"sex\">" +
-    "<option>Mężczyzna<\/option><option>Kobieta<\/option><\/select><\/div><div class=\"form-group\">" +
-    "<input type=\"submit\" class=\"btn btn-primary\" value=\"Zapisz\">" +
-    "<button type=\"button\" id=\"cancelBtn\" class=\"btn btn-warning\">Cofnij<\/button><\/div><\/form><\/div>" +
-    "<div class=\"col-md-4\">&nbsp;<\/div><\/div>"];
