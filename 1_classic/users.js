8872b5c82c0680004b9a9eaa4271d5f3c5b32ff3
Filip Olszewski <filipolszew@o2.pl>
2016-08-13 12:11:03 +0200

    working version v1
    + refactors

--- 1_classic/users.js
+++ 1_classic/users.js
@@ -1,17 +1,20 @@
 var users = [["Jacek", "Doe", "43", "Mężczyzna"],
     ["Marzanna", "Uss", "54", "Kobieta"], ["Julia", "Dolej", "22", "Kobieta"]];
 
-var selectedRow = -1;
-var isAnyRowSelected = false;
 
+var isAnyRowSelected;
+var selectedRow;
 
 function createTableView(users) {
-
+    clearWorkspace();
+    isAnyRowSelected = false;
+    selectedRow = -1;
     var buttonHtml = "<button type=\"button\" class=\"btn btn-primary\" id=\"addBtn\">Dodaj<\/button> " +
         "<button type=\"button\" class=\"btn btn-primary\" id=\"editBtn\" disabled>Popraw<\/button> " +
         "<button type=\"button\" class=\"btn btn-primary\" id=\"deleteBtn\" disabled>Usuń<\/button>";
 
     $("#workspace").html(buttonHtml + prepareTableHtml(users) + '<div id="extendedView"></div>');
+    setOnClickForButtons();
 }
 
 function prepareTableHtml(users) {
@@ -56,34 +59,86 @@ function onRowClick(rowNumber) {
 }
 
 function showExtendedView(rowNumber) {
+    var extViewHtml =
+        ['<div class="container"><div class="row"><div class="col-md-2 evLabel">Imie:</div><div class="col-md-2 evValue">',
+            '</div></div><div class="row"><div class="col-md-2 evLabel">Nazwisko:</div><div class="col-md-2 evValue">',
+            '</div></div><div class="row"><div class="col-md-2 evLabel">Wiek:</div><div class="col-md-2 evValue">',
+            '</div></div><div class="row"><div class="col-md-2 evLabel">Płeć:</div><div class="col-md-2 evValue">',
+            '</div></div></div>'];
+
     var user = users[rowNumber];
     $("#extendedView").html(extViewHtml[0] + user[0]
         + extViewHtml[1] + user[1] + extViewHtml[2] + user[2] + extViewHtml[3] + user[3] + extViewHtml[4]);
 }
 
-var extViewHtml =
-    ['<div class="container"><div class="row"><div class="col-md-2 evLabel">Imie:</div><div class="col-md-2 evValue">',
-        '</div></div><div class="row"><div class="col-md-2 evLabel">Nazwisko:</div><div class="col-md-2 evValue">',
-        '</div></div><div class="row"><div class="col-md-2 evLabel">Wiek:</div><div class="col-md-2 evValue">',
-        '</div></div><div class="row"><div class="col-md-2 evLabel">Płeć:</div><div class="col-md-2 evValue">',
-        '</div></div></div>'];
+function setOnClickForButtons() {
+    $("#addBtn").click(function () {
+        clearWorkspace();
+        createFormView("Dodaj");
+    });
+    $("#editBtn").click(function () {
+        clearWorkspace();
+        createFormView("Edytuj");
+    });
+    $("#deleteBtn").click(function () {
+        var answer = confirm("Czy chcesz usunąć tego użytkownika?");
+        if (answer) {
+            deleteUser();
+        }
+    });
+}
+
+function clearWorkspace() {
+    $("#workspace").html("");
+}
+
+function createFormView(mode) {
+    $("#workspace").html(formHtml[0] + mode + " Użytkownika" + formHtml[1]);
+    if(mode == "Edytuj"){
+        var user = users[selectedRow];
+        $("#name").val(user[0]);
+        $("#surname").val(user[1]);
+        $("#age").val(user[2]);
+        $("#sex").val(user[3]);
+        $("#form").on("submit", onEditFormSubmit);
+    } else {
+        $("#form").on("submit", onAddFormSubmit);
+    }
+    $("#cancelBtn").click(function () {
+        createTableView(users);
+    });
+}
+
+function onEditFormSubmit() {
+    var i = selectedRow;
+    users[i][0] = $("#name").val();
+    users[i][1] = $("#surname").val();
+    users[i][2] = $("#age").val();
+    users[i][3] = $("#sex").val();
+    createTableView(users);
+    return false;
+}
+
+function onAddFormSubmit() {
+    var user = [];
+    user[0] = $("#name").val();
+    user[1] = $("#surname").val();
+    user[2] = $("#age").val();
+    user[3] = $("#sex").val();
+    users.push(user);
+    createTableView(users);
+    return false;
+}
 
 function deleteUser() {
     users.splice(selectedRow, 1);
     selectedRow = -1;
     isAnyRowSelected = false;
-    createTable(users);
+    createTableView(users);
 }
 
 function initialize() {
     getData();
-
-    $("#deleteBtn").click(function () {
-        var answer = confirm("Czy chcesz usunąć tego użytkownika?");
-        if (answer) {
-            deleteUser(); //imitacja
-        }
-    });
 }
 function getData() {
     setTimeout(createTableView(users), 300);
@@ -92,14 +147,13 @@ function getData() {
 $(document).ready(initialize);
 
 
-var formHtml = ["<div class=\"row\"><div class=\"col-md-4\">&nbsp;<\/div><div class=\"col-md-4\">" +
-"<h3>Dodaj użytkownika<\/h3><form name=\"form\" id=\"form\" onsubmit=\"on",
-    "FormSubmit()\"><div class=\"form-group\"><label for=\"name\">Imie:<\/label>" +
+var formHtml = ["<div class=\"row\"><div class=\"col-md-4\">&nbsp;<\/div><div class=\"col-md-4\"><h3>",
+    "<\/h3><form name=\"form\" id=\"form\"><div class=\"form-group\"><label for=\"name\">Imie:<\/label>" +
     "<input id=\"name\" class=\"form-control\" type=\"text\"><\/div><div class=\"form-group\">" +
     "<label for=\"surname\">Nazwisko:<\/label><input id=\"surname\" class=\"form-control\" type=\"text\"><\/div>" +
     "<div class=\"form-group\"><label for=\"age\">Wiek:<\/label><input id=\"age\" class=\"form-control\" type=\"text\"><\/div>" +
     "<div class=\"form-group\"><label for=\"sex\">Płeć:<\/label><select class=\"form-control\" id=\"sex\">" +
     "<option>Mężczyzna<\/option><option>Kobieta<\/option><\/select><\/div><div class=\"form-group\">" +
-    "<input type=\"submit\" class=\"btn btn-primary\" value=\"Dodaj\">" +
+    "<input type=\"submit\" class=\"btn btn-primary\" value=\"Zapisz\">" +
     "<button type=\"button\" id=\"cancelBtn\" class=\"btn btn-warning\">Cofnij<\/button><\/div><\/form><\/div>" +
     "<div class=\"col-md-4\">&nbsp;<\/div><\/div>"];
