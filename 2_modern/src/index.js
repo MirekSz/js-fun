c7b57db62bfa27ca5ffc2d03bcbb39cbecb786a2
filipolszew <filipolszew@o2.pl>
2016-08-17 18:18:47 +0200

    jquery test

--- 2_modern/src/index.js
+++ 2_modern/src/index.js
@@ -1,4 +1,9 @@
 import people from './people.js';
+import $ from 'jquery';
+$(document).ready(initialize);
 
-var [ person, person2 ] = people;
-console.log(person);
+function initialize() {
+    var [ person, person2 ] = people;
+    console.log(person);
+    document.getElementById('workspace').innerHTML = `${person} and ${person2}`;
+}
