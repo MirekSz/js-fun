57f2bfcb581a3c1b733266228feb7a9ba73fd5ee
filipolszew <filipolszew@o2.pl>
2016-08-29 19:46:41 +0200

    Gulp less-to-css - logging processed file

--- 2_modern/gulpfile.js
+++ 2_modern/gulpfile.js
@@ -1,10 +1,20 @@
-var gulp = require('gulp'),
-    notify = require('gulp-notify'),
-    less = require('gulp-less');
+'use strict';
+
+let gulp = require('gulp');
+let notify = require('gulp-notify');
+let less = require('gulp-less');
+let through = require('through2');
 
 gulp.task('less-to-css', function () {
     return gulp.src('./*.less')
+        .pipe(through.obj((file, enc, callback) => {
+            console.log(`Processing ${file.relative} file.`);
+            callback(null, file);
+        }))
         .pipe(less('./users.less', {style: 'expanded'}))
         .pipe(gulp.dest('./dist/css'))
+
         .pipe(notify({message: 'Less-to-css task complete'}));
 });
+
+
