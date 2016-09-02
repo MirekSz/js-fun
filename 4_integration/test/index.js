db2409b69df3bc158511815a97a4c2dddc9a35e8
MirekSz <m.szajowski@gmail.com>
2016-09-02 10:48:52 +0200

    mocha chai config

--- 4_integration/test/index.js
+++ 4_integration/test/index.js
@@ -1,3 +1,16 @@
+
+global.ita=function(description, callback) {
+    return it(description,
+        async(done) => {
+            try {
+                await callback();
+                done();
+            } catch (err) {
+                done(err);
+            }
+        }
+    )
+}
 let context = require.context('.', true, /.+\.test\.js?$/);
 context.keys().forEach(context);
 module.exports = context;
