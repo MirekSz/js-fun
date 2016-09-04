64de93f6831fe90cecc6027cc3c8753232336b17
filipolszew <filipolszew@o2.pl>
2016-09-04 15:17:31 +0200

    more tests (working)

--- 4_integration/test/UserService.test.js
+++ 4_integration/test/UserService.test.js
@@ -1,7 +1,7 @@
 /*eslint-disable */
 
 import EventEmitter from 'event-emitter';
-import UserService from '../src/UserService';
+import UserService, {USER_SERVICE_EVENT} from '../src/UserService';
 
 
 let baseUrl = 'http://localhost:3000';
@@ -23,5 +23,42 @@ describe('UserService tests...', function () {
         users = await service.getUsers();
         var lastSize = users.length;
         expect(lastSize).to.be.eq(currentSize + 1);
+    });
+    it('should add user', () => {
+        //given
+        var promise = service.getUsers();
+        promise.then((users) => {
+            var currentSize = users.length;
+            service.addUser({name: 'Jan'}).then(() => {
+                service.getUsers().then((users) => {
+                    var lastSize = users.length;
+                    expect(1).to.be.eq(currentSize + 1);
+                });
+            })
+        });
+    });
+    it('should fire event after load users', (done) => {
+        //given
+        let users = [];
+        ee.on(USER_SERVICE_EVENT.USERS_NEW_DATA, (data)=> {
+            users = data;
+        });
+
+        var userService = new UserService(ee);
+        userService.http = {
+            getData: ()=> {
+                return Promise.resolve({data: [{}]});
+            }
+        };
+
+        //when
+        userService.getUsers().then(()=>{
+        expect(users).to.not.be.empty;
+
+        }).then(done).catch(done);
+
+        //thenn
+        // expect(users).to.not.be.empty; expect z sinona
+
     });
 });
