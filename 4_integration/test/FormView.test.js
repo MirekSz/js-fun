12d465ea4f7c9d9d6638c80e41daceedd907da5c
filipolszew <filipolszew@o2.pl>
2016-09-06 22:56:25 +0200

    testview tests - passing

--- 4_integration/test/FormView.test.js
+++ 4_integration/test/FormView.test.js
@@ -4,6 +4,7 @@ import EventEmitter from 'event-emitter';
 import FormView from '../src/FormView';
 import EventRouter from '../src/EventRouter';
 import {BUTTON_EVENTS} from '../src/ButtonView';
+import {TABLE_EVENTS} from '../src/TableView';
 
 describe('FormView Tests...', function () {
     let ee = new EventEmitter();
@@ -18,6 +19,36 @@ describe('FormView Tests...', function () {
         $('#workspace').empty();
     });
 
+    it('should deserialize', () => {
+        //given
+        formView.render('#workspace', 'Dodaj');
+        let user = {id: 0, name: "Jacek", surname: "Doe", age: "43", sex: "Mężczyzna"};
+        let $form = $('#form');
+
+        //when
+        formView.deserializeForm($form, user);
+        let name = $form.find(`input[name='name']`).val();
+        let sex = $form.find(`select[name='sex']`).val();
+
+        //then
+        expect(name).to.equal('Jacek');
+        expect(sex).to.equal('Mężczyzna');
+    });
+
+    it('should serialize', () => {
+        //given
+        formView.render('#workspace', 'Dodaj');
+        let user = {id: 0, name: "Jacek", surname: "Doe", age: "43", sex: "Mężczyzna"};
+        let $form = $('#form');
+        formView.deserializeForm($form, user);
+
+        //when
+        let serializedUser = formView.serializeForm($form, {id: 0});
+
+        //then
+        expect(serializedUser).eql(user);
+    });
+
     it('should render on ADD_NEW_USER', () => {
         //given
         router.setFormView(formView);
@@ -29,20 +60,24 @@ describe('FormView Tests...', function () {
 
         //then
         expect(spy).to.have.been.calledWith('#workspace', 'Dodaj');
+        spy.restore();
     });
-    it('should serialize and deserialize', ()=> {
+
+    it('should render on EDIT_USER and deserialize', () => {
         //given
-        formView.render('#workspace', 'Dodaj');
+        router.setFormView(formView);
+        let spy = sinon.spy(formView, 'render');
+        router.start();
         let user = {id: 0, name: "Jacek", surname: "Doe", age: "43", sex: "Mężczyzna"};
-        let $form = $('#form');
 
         //when
-        formView.deserializeForm($form, user);
-        let serializedUser = formView.serializeForm($form, user);
+        ee.emit(TABLE_EVENTS.EDIT_USER, {id: 0, name: "Jacek", surname: "Doe", age: "43", sex: "Mężczyzna"});
+        let $form = $('#form');
+        let serializedUser = formView.serializeForm($form, {id:0});
 
         //then
-        expect(serializedUser).to.be.eq(user);
+        expect(spy).to.have.been.calledWith('#workspace', 'Edytuj');
+        expect(serializedUser).eql(user);
+        spy.restore();
     });
-
-
 });
