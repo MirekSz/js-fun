3467dbb010212da2c60ba53de8e76db60dce1530
filipolszew <filipolszew@o2.pl>
2016-09-21 19:27:15 +0200

    basic table search

--- 5_angular-framework/5_angular_iteration/app/js/controllers.js
+++ 5_angular-framework/5_angular_iteration/app/js/controllers.js
@@ -6,7 +6,7 @@ var phonecatControllers = angular.module('phonecatControllers', []);
 
 phonecatControllers.controller('UsersTableCtrl', ['$scope',
     function ($scope) {
-        $scope.users = [{id: 2, name: 'John', surname: 'Doe', age: 43, sex: 'Mężczyzna'},
+        $scope.users = [{id: 2, name: 'Anna', surname: 'Doex', age: 43, sex: 'Kobieta'},
             {id: 3, name: 'John', surname: 'Doe', age: 45, sex: 'Mężczyzna'}];
         $scope.loading = false;
         $scope.isUserSelected = false;
